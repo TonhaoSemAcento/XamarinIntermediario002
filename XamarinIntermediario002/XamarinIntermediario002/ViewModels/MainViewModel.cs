02596a21237d5ae1ef06e8086395370d244c8b09
Antônio Rafael Santos Prestes <tonhaosemacento@gmail.com>
2017-05-05 13:58:24 -0300

    Add project files.

---
+++ XamarinIntermediario002/XamarinIntermediario002/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinIntermediario002.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _propriedadeTexto;
+
+        public string PropriedadeTexto
+        {
+            get { return _propriedadeTexto; }
+            set { SetProperty(ref _propriedadeTexto, value);    }
+        }
+
+    }
+}
